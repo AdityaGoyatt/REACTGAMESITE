d75d3ba18dfbb8fcc457351912c07947a47fb528
Aditya <aditya.goyat023@gmail.com>
2023-05-30 17:20:36 -0700

    using GitIgnore

--- src/Components/apiClient.tsx
+++
@@ -1,8 +0,0 @@
-import axios, { CanceledError } from "axios";
-
-export default axios.create({
-  baseURL: "https://api.rawg.io/api",
-  params: {
-    key: "beebe2c2a1f747258de36088d3bff45c",
-  },
-});
