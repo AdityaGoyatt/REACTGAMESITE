7355aac1420abc3238752ed0c65e325b15889464
Aditya <aditya.goyat023@gmail.com>
2023-05-31 11:25:30 -0700

    Dark Mode Config Added

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -6,27 +6,43 @@ import {
   FormControl,
   FormLabel,
   Switch,
+  HStack,
+  Image,
+  Text,
 } from "@chakra-ui/react";
+
+import logo from "./images/logo.svg";
 
 interface Props {
   clickLogic: () => void;
 }
 const NavBAr = ({ clickLogic }: Props) => {
   return (
-    <Flex m={2} minWidth="max-content" alignItems="center" gap="2">
-      <Heading size="md">Chakra App</Heading>
+    <HStack>
+      <Image src={logo} boxSize="60px"></Image>
       <Spacer />
-      <FormControl display="flex" alignItems="center">
-        <Spacer />
-        <FormLabel htmlFor="dark-mode" mb="0">
-          <Heading size="md" color="tomato">
-            Dark Mode
-          </Heading>
-        </FormLabel>
-        <Switch onClick={clickLogic} id="dark-mode" />
-      </FormControl>
-    </Flex>
+      <FormLabel htmlFor="dark-mode" mb="0">
+        <Text>
+          Dark Mode
+          <Switch onClick={clickLogic} id="dark-mode" />
+        </Text>
+      </FormLabel>
+    </HStack>
   );
 };
 
 export default NavBAr;
+
+// <Flex m={2} minWidth="max-content" alignItems="center" gap="2">
+//   <Heading size="md">Chakra App</Heading>
+//   <Spacer />
+//   <FormControl display="flex" alignItems="center">
+//     <Spacer />
+//     <FormLabel htmlFor="dark-mode" mb="0">
+//       <Heading size="md" color="tomato">
+//         Dark Mode
+//       </Heading>
+//     </FormLabel>
+//     <Switch onClick={clickLogic} id="dark-mode" />
+//   </FormControl>
+// </Flex>
