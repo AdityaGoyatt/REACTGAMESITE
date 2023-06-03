1052fad750b71f06a0810dd6d886baeb70816bd9
Aditya <aditya.goyat023@gmail.com>
2023-06-03 16:46:51 -0700

    before Useing Platform in games hook

--- src/Components/PlatformSelector.tsx
+++ src/Components/PlatformSelector.tsx
@@ -1,8 +1,12 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
+import { Platform } from "../hooks/usePlatforms";
+interface Props {
+  handleClick: (platform: Platform) => void;
+}
 
-const PlatformSelector = () => {
+const PlatformSelector = ({ handleClick }: Props) => {
   const { dataList, error } = usePlatforms();
   if (error) return null;
   return (
@@ -12,7 +16,9 @@ const PlatformSelector = () => {
       </MenuButton>
       <MenuList>
         {dataList.map((platform) => (
-          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem onClick={() => handleClick(platform)} key={platform.id}>
+            {platform.name}
+          </MenuItem>
         ))}
       </MenuList>
     </Menu>
