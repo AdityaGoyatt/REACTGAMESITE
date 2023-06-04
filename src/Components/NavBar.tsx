34c22e166b801abf76379b7a0c16791e3b50848e
Aditya <aditya.goyat023@gmail.com>
2023-06-04 10:39:52 -0700

    searchBar added and functionality setted up

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -13,13 +13,18 @@ import {
 
 import logo from "./images/logo.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchBar from "./SearchBar";
+
+interface Props {
+  handleClick: (searchText: string) => void;
+}
 
 interface Props {}
-const NavBAr = () => {
+const NavBAr = ({ handleClick }: Props) => {
   return (
     <HStack p={2} justifyContent="space-between">
       <Image src={logo} boxSize="60px"></Image>
-
+      <SearchBar handleClick={(searchText) => handleClick(searchText)} />
       <ColorModeSwitch />
     </HStack>
   );
