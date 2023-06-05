d2f73bf82c67eae1670d4314b701d0f65c7352b1
Aditya <aditya.goyat023@gmail.com>
2023-06-04 17:56:41 -0700

    emojis added and basic refactoring done

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -19,6 +19,7 @@ export interface game {
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   genres: Array<genre>;
+  rating_top: number;
 }
 
 const useGames = (gameQuery: gameQuery) =>
