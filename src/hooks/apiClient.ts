c7a7d45a064fdbe5d1296a968e20affc601ea0b9
Aditya <aditya.goyat023@gmail.com>
2023-06-06 21:58:19 -0700

    resolved the environment variable issue

--- src/hooks/apiClient.ts
+++ src/hooks/apiClient.ts
@@ -1,9 +1,8 @@
 import axios, { CanceledError } from "axios";
 
-const key = import.meta.env.VITE_APP_APICLIENT;
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: import.meta.env.VITE_API_APICLIENT,
+    key: import.meta.env.VITE_API_KEY,
   },
 });
